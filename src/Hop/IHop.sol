d3e722e0831db09e5153a407b823f1d3b8d7acbd
shivam2320 <alekarshivam23@gmail.com>
2023-01-15 11:50:45 +0530

    Hop init

---
+++ src/Hop/IHop.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.0;
+
+/**
+@title HopAMM
+@notice responsible for calling the HOP L2 Impl functions.
+ */
+interface IHop {
+    function calculateSwap(
+        uint8 tokenIndexFrom,
+        uint8 tokenIndexTo,
+        uint256 dx
+    ) external view returns (uint256);
+
+    function swap(
+        uint8 tokenIndexFrom,
+        uint8 tokenIndexTo,
+        uint256 dx,
+        uint256 minDy,
+        uint256 deadline
+    ) external returns (uint256);
+
+    function swapAndSend(
+        uint256 chainId,
+        address recipient,
+        uint256 amount,
+        uint256 bonderFee,
+        uint256 amountOutMin,
+        uint256 deadline,
+        uint256 destinationAmountOutMin,
+        uint256 destinationDeadline
+    ) external payable;
+
+    function sendToL2(
+        uint256 chainId,
+        address recipient,
+        uint256 amount,
+        uint256 amountOutMin,
+        uint256 deadline,
+        address relayer,
+        uint256 relayerFee
+    ) external payable;
+
+    function getTokenIndex(address tokenAddress) external view returns (uint8);
+}
