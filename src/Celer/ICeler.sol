43951fcb58ffabbad1f0c939c450444b267a99cc
shivam2320 <alekarshivam23@gmail.com>
2023-01-20 23:34:51 +0530

    Celer interface

---
+++ src/Celer/ICeler.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.0;
+
+interface ICeler {
+    function send(
+        address _receiver,
+        address _token,
+        uint256 _amount,
+        uint64 _dstChinId,
+        uint256 _nonce,
+        uint32 _maxSlippage
+    ) external;
+
+    function sendNative(
+        address _receiver,
+        uint256 _amount,
+        uint64 _dstChinId,
+        uint256 _nonce,
+        uint32 _maxSlippage
+    ) external payable;
+}
